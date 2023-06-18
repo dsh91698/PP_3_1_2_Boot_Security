375715e1f8d873ee1ea5ae1dd8f3b06942968a2c
dsh91698 <mettus@mail.ru>
2023-06-19 02:00:38 +0400

    refactor: create three users - 2 with admin, user only roles and 1 admin+user roles

--- src/main/java/ru/kata/spring/boot_security/demo/util/AddUsersToDb.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/AddUsersToDb.java
@@ -2,8 +2,12 @@ package ru.kata.spring.boot_security.demo.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AddUsersToDb {
@@ -11,10 +15,20 @@ public class AddUsersToDb {
 
     @Autowired
     public AddUsersToDb(UserService userService) {
+        List<Role> userRole = new ArrayList<>();
+        userRole.add(new Role("USER"));
+
+        List<Role> adminRole = new ArrayList<>();
+        adminRole.add(new Role("ADMIN"));
+
+        List<Role> adminAndUserRoles = new ArrayList<>();
+        adminAndUserRoles.add(new Role("ADMIN"));
+        adminAndUserRoles.add(new Role("USER"));
+
         this.userService = userService;
-        userService.addUser(new User("Anand Baulk", "active", 25));
-//        userService.addUser(new User("Caleb Clemens", "active", 35));
-//        userService.addUser(new User("Noah Newton", "registered", 15));
+        userService.addUser(new User("Anand Baulk", "active", 25, "anand","anand", adminAndUserRoles));
+        userService.addUser(new User("Caleb Clemens", "active", 15, "user","user", userRole));
+        userService.addUser(new User("Noah Newton", "active", 45, "admin","admin", adminRole));
     }
 
 }
