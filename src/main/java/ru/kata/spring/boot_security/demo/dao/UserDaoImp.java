b8e887e4dc927aaacf058a896db494d8a791a0b9
dsh91698 <mettus@mail.ru>
2023-06-21 17:49:26 +0400

    refactor: move password encode intp service level

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImp.java
@@ -3,7 +3,7 @@ package ru.kata.spring.boot_security.demo.dao;
 //import jakarta.persistence.EntityManager;
 //import jakarta.persistence.PersistenceContext;
 //import jakarta.persistence.TypedQuery;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.User;
 
@@ -11,21 +11,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
+
 //@Transactional
 @Repository
 public class UserDaoImp implements UserDao {
-    private final PasswordEncoder passwordEncoder;
     @PersistenceContext
     private EntityManager entityManager;
 
-    public UserDaoImp(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
     @Override
     public void addUser(User user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
         entityManager.persist(user);
     }
 
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,25 +1,31 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
+
 @Transactional
 @Service
 public class UserServiceImpl implements UserService {
+    private final PasswordEncoder passwordEncoder;
 
     private UserDao userDao;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserDao userDao) {
+        this.passwordEncoder = passwordEncoder;
         this.userDao = userDao;
     }
 
     @Override
     public void addUser(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userDao.addUser(user);
     }
 
