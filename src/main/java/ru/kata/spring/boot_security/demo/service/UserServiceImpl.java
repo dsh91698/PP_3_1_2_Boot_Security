5a736b1f4b1f4d2efa7691f2b1e2d08b0a86f716
dsh91698 <mettus@mail.ru>
2023-06-21 22:34:04 +0400

    fix: password issue fix: when user details update, password correctly update too no matter change it or not

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -5,8 +5,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Transactional
@@ -24,6 +27,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void addUser(User user) {
+        if (user.getRoles() == null) { // USER role set by default
+            user.setRoles(Arrays.asList(new Role("ROLE_USER")));
+        }//TODO:
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
         userDao.addUser(user);
@@ -41,6 +47,22 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updateUser(User user) {
+        User userFromDB = userDao.getById(user.getId());
+
+        System.out.println("userFromDB.getPassword() ->" + userFromDB.getPassword());
+        System.out.println("user.getPassword() ->" + user.getPassword());
+        System.out.println("user ->" + user);
+
+        user.setRoles(new ArrayList<>(userFromDB.getRoles()));//TODO:
+
+        boolean hasNewPassword =  user.getPassword() != "";// is there new password in input?
+        if (hasNewPassword) {// if something was typed in password field - encode password and add into updated user
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+        }
+        else { // if there was no user input in password field - extract old password from DB and add into updated user
+            user.setPassword(userFromDB.getPassword()); // Set the original password if it hasn't changed
+        }
         userDao.updateUser(user);
     }
 
@@ -48,4 +70,5 @@ public class UserServiceImpl implements UserService {
     public void deleteById(Long id) {
         userDao.deleteById(id);
     }
+
 }
