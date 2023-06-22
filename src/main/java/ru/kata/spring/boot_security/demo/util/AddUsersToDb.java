88ca344f7bec7317bb890c3a05e85114e7715974
dsh91698 <mettus@mail.ru>
2023-06-23 00:01:04 +0400

    feat: add mock users into DB

--- src/main/java/ru/kata/spring/boot_security/demo/util/AddUsersToDb.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/AddUsersToDb.java
@@ -4,35 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class AddUsersToDb {
     private UserService userService;
+    private RoleService roleService;
 
     @Autowired
-    public AddUsersToDb(UserService userService) {
-        List<Role> userRole = new ArrayList<>();
-        userRole.add(new Role("ROLE_USER"));
-//        userRole.add(new Role("USER"));
-
-        List<Role> adminRole = new ArrayList<>();
-        adminRole.add(new Role("ROLE_ADMIN"));
-//        adminRole.add(new Role("ADMIN"));
-
-        List<Role> adminAndUserRoles = new ArrayList<>();
-        adminAndUserRoles.add(new Role("ROLE_ADMIN"));
-//        adminAndUserRoles.add(new Role("ADMIN"));
-        adminAndUserRoles.add(new Role("ROLE_USER"));
-//        adminAndUserRoles.add(new Role("USER"));
-
+    public AddUsersToDb(UserService userService, RoleService roleService) {
         this.userService = userService;
-        userService.addUser(new User("Anand Baulk", "active", 25, "anand","anand", adminAndUserRoles));
-        userService.addUser(new User("Caleb Clemens", "active", 15, "user","user", userRole));
-        userService.addUser(new User("Noah Newton", "active", 45, "admin","admin", adminRole));
+        this.roleService = roleService;
+        //create roleList and fill in Roles-table in DB
+        List<Role> roleList = new ArrayList<>(Arrays.asList(new Role("ROLE_USER"), new Role("ROLE_ADMIN")));
+        roleService.addRolesToDb(roleList);
+
+        List<Role> userRole = Arrays.asList(roleService.getRoleByName("ROLE_USER"));
+
+        List<Role> adminRole = Arrays.asList(roleService.getRoleByName("ROLE_ADMIN"));
+
+        List<Role> adminAndUserRoles = Arrays.asList(roleService.getRoleByName("ROLE_USER"), roleService.getRoleByName("ROLE_ADMIN"));
+
+        userService.addUser(new User("Anand Baulk", "active", 25, "anand", "anand", adminAndUserRoles));
+        userService.addUser(new User("Caleb Clemens", "active", 15, "user", "user", userRole));
+        userService.addUser(new User("Noah Newton", "active", 45, "admin", "admin", adminRole));
     }
 
 }
