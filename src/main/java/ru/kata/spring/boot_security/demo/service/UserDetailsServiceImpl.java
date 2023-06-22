b49989210e7d8014ca8caa8f7399de9100af3471
dsh91698 <mettus@mail.ru>
2023-06-23 00:03:46 +0400

    fix: LAZY load work now

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import org.hibernate.Hibernate;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.User;
 
@@ -17,11 +20,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
+    @Transactional
+//    @Query("SELECT DISTINCT user FROM User user LEFT JOIN FETCH user.roles WHERE user.userName = :username")
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userDao.findByLogin(username);
         if (user == null) {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
+        Hibernate.initialize(user.getRoles());
         return user;
     }
 }
